690cfd6002d16c1ee126761190ea212623d5eb7e
Kestrel.Bryce <99274358+KestrelBryce@users.noreply.github.com>
2022-04-05 15:05:17 +0000

    It lives :)

--- js/script.js
+++ js/script.js
@@ -1,24 +1,30 @@
-  /* This function calculates the exchange rate for CAD to AUD. 
+  /* This function calculates the exchange rate for CAD to AUD and CAD to GBP. 
 */
 
-function calculate() {
+function calculateAUD() {
   // input
   let CAD_AUD = document.getElementById('CAD-to-AUD-here').value;
-  //let CAD_GBP = parseInt(document.getElementById('CAD-to-GBP-here').value);
-
+  
   // naming constants and stuff
-  //const AUD_RATE = 1.0667808;
-  //const GBP_RATE = 0.6101231;
-  //let AUDConversion = CAD_AUD * AUD_RATE;
-  //let GBPConversion = CAD_GBP * GBP_RATE;
+  const AUD_RATE = 1.0667808;
+  let AUDConversion = CAD_AUD * AUD_RATE;
 
   // remember to use .toFixed(2) to round to two decimal places
-//testing
-document.getElementById('AUDResults').innerHTML = "testing"
   
-  // output for CAD to AUD
-  document.getElementById('AUDConversion').innerHTML = "insert words here" + AUDConversion.toFixed(2) + "yes"
+ // output for CAD to AUD
+document.getElementById('AUDResults').innerHTML = "Your Canadian dollars add up to $" + AUDConversion.toFixed(2) + " Australian dollars."
+}
 
-  // output for CAD to GBP
- // document.getElementById('GBPConversion').innerHTML = "insert words here"
+function calculateGBP() {
+  // input
+  let CAD_GBP = (document.getElementById('CAD-to-GBP-here').value);
+
+  // naming constants and stuff
+  const GBP_RATE = 0.6101231;
+  let GBPConversion = CAD_GBP * GBP_RATE;
+
+  // remember to use .toFixed(2) to round to two decimal places
+  
+ // output for CAD to GBP
+document.getElementById('GBPResults').innerHTML = "Your Canadian dollars add up to £" + GBPConversion.toFixed(2) + " British pounds."
 }
